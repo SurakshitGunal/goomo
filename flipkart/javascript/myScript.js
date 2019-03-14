56f98e51dd30cf7a038ddb27b181277e460c55bb
surakshit.gunal <surakshit.gunal@goomo.com>
2019-03-14 17:37:20 +0530

    update of flipkart

--- flipkart/javascript/myScript.js
+++ flipkart/javascript/myScript.js
@@ -1,13 +1,32 @@
-function fname() {
-  var firstName = document.getElementById("fn").value;
-  console.log("fn");
-  debugger;
-  if (fn == "") {
-    alert("Name must be filled out");
-    return false;
+
+ 
+ function fname(){
+   var firstname = document.getElementById("fn").value;
+   document.getElementById("outputfn").innerHTML= firstname;
+
+    var lastname = document.getElementById("ln").value;
+    document.getElementById("outputln").innerHTML=lastname;
+
+    var fullname = firstname  + lastname ;
+    document.getElementById("outputfl").innerHTML=fullname; 
+     
+   
   }
-  else {
-    alert("good work");
-    document.getElementById("output").innerHTML = "name";
+  function mfgender(){
+    var male = document.getElementsByName("gender")[0].checked;
+    if (!male){
+      document.getElementById("outputgender").innerHTML= "female";
+    }
+    else{
+      document.getElementById("outputgender").innerHTML= "male";
+    }
   }
-}
+  function mail(){
+    var mail = document.getElementById("email").value;
+    document.getElementById("outputmail").innerHTML= mail;
+  }
+  function mbn(){
+    var mn = document.getElementById("mb").value;
+    document.getElementById("outputnu").innerHTML= mn;
+  }
+  
