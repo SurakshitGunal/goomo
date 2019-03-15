629aa9943ccbc4981690984e06fe396043837628
surakshit.gunal <surakshit.gunal@goomo.com>
2019-03-15 15:43:53 +0530

    updated flipkart site

--- flipkart/javascript/myScript.js
+++ flipkart/javascript/myScript.js
@@ -1,13 +1,39 @@
-function fname() {
-  var firstName = document.getElementById("fn").value;
-  console.log("fn");
-  debugger;
-  if (fn == "") {
-    alert("Name must be filled out");
-    return false;
+
+ 
+ function show() {
+  
+ }
+ function fname(){
+   var firstname = document.getElementById("fn").value;
+   document.getElementById("outputfn").innerHTML= "first name" + firstname;
+
+    var lastname = document.getElementById("ln").value;
+    document.getElementById("outputln").innerHTML=lastname;
+
+    var fullname = firstname  + lastname ;
+    document.getElementById("outputfl").innerHTML=fullname; 
+     
+    var male = document.getElementsByName("gender")[0].checked;
+    if (!male){
+      document.getElementById("outputgender").innerHTML= "female";
+    }
+    else{
+      document.getElementById("outputgender").innerHTML= "male";
+    }
+    var x=document.getElementById("email1").value;  
+    var atposition=x.indexOf("@");  
+    var dotposition=x.lastIndexOf(".");  
+    if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+      alert("Please enter a valid e-mail address \n atpostion:"+atposition+"\n dotposition:"+dotposition);  
+      return false;  
+      }    
+    document.getElementById("outputmail").innerHTML= x
+    var mn = document.getElementById("mb").value;
+    if(mb.value == "") {
+      window.alert("Error: Cell number must not be null.");
+      number.focus();
+      return false;
   }
-  else {
-    alert("good work");
-    document.getElementById("output").innerHTML = "name";
+    document.getElementById("outputnu").innerHTML= mn;
   }
-}
+  
