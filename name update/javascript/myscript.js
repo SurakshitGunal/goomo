594ea9f9867b18619c18ab83ded78380fcba7a2c
surakshit.gunal <surakshit.gunal@goomo.com>
2019-04-11 14:42:00 +0530

    task 2 name filler

---
+++ name update/javascript/myscript.js
@@ -0,0 +1,11 @@
+var fname ;
+var myName;
+var outputName;
+function fname(){
+    if(nameid=null){
+        window.alert(EnterName);
+    } else{
+    var myName = document.getElementById("nameid").value;
+    document.getElementById("outputName").innerHTML =myName; 
+}
+}
